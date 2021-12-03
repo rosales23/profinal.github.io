ee2bbe93e559b07b8c42005f265af6d802cdce01
rosales23 <rosales.upi@gmail.com>
2021-12-03 12:39:40 -0600

    menu

--- js/CtrlMenu.js
+++ js/CtrlMenu.js
@@ -1,96 +1,170 @@
 import {
-    getAuth,
-    getFirestore
-  } from "../lib/fabrica.js";
-  import {
-    eliminaStorage,
-    urlStorage
-  } from "../lib/storage.js";
-  import {
-    muestraError
-  } from "../lib/util.js";
-  import {
-    muestraUsuarios
-  } from "./navegacion.js";
-  import {
-    tieneRol
-  } from "./seguridad.js";
-  import {
-    checksRoles,
-    guardaUsuario,
-    selectAlumnos
-  } from "./usuarios.js";
-  
-  const params =
-    new URL(location.href).
-      searchParams;
-  const id = params.get("id");
-  const daoUsuario = getFirestore().
-    collection("Usuario");
-  /** @type {HTMLFormElement} */
-  const forma = document["forma"];
-  const img = document.
-    querySelector("img");
-  /** @type {HTMLUListElement} */
-  const listaRoles = document.
-    querySelector("#listaRoles");
-  getAuth().onAuthStateChanged(
-    protege, muestraError);
-  
-  /** @param {import(
-      "../lib/tiposFire.js").User}
-      usuario */
-  async function protege(usuario) {
-    if (tieneRol(usuario,
-      ["Cliente"])) {
-      busca();
-    }
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  urlStorage
+} from "../lib/storage.js";
+import {
+  cod,
+  muestraError
+} from "../lib/util.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+
+/** @type {HTMLUListElement} */
+// @ts-ignore
+const lista = document.
+  querySelector("#lista");
+const firestore = getFirestore();
+const daoRol = firestore.
+  collection("Rol");
+const daoAlumno = firestore.
+  collection("Alumno");
+const daoUsuario = firestore.
+  collection("Usuario");
+
+getAuth().onAuthStateChanged(
+  protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Cliente"])) {
+    consulta();
   }
-  
-  async function busca() {
-    try {
-      const doc = await daoUsuario.
+}
+
+function consulta() {
+  daoUsuario.onSnapshot(
+    htmlLista, errConsulta);
+}
+
+/**
+ * @param {import(
+    "../lib/tiposFire.js").
+    QuerySnapshot} snap */
+async function htmlLista(snap) {
+  let html = "";
+  if (snap.size > 0) {
+    /** @type {
+          Promise<string>[]} */
+    let usuarios = [];
+    snap.forEach(doc => usuarios.
+      push(htmlFila(doc)));
+    const htmlFilas =
+      await Promise.all(usuarios);
+    /* Junta el todos los
+     * elementos del arreglo en
+     * una cadena. */
+    html += htmlFilas.join("");
+  } else {
+    html += /* html */
+      `<li class="vacio">
+        -- No hay usuarios
+        registrados. --
+      </li>`;
+  }
+  lista.innerHTML = html;
+}
+
+/**
+ * @param {import(
+    "../lib/tiposFire.js").
+    DocumentSnapshot} doc */
+async function htmlFila(doc) {
+  /**
+   * @type {import("./tipos.js").
+                      Usuario} */
+  const data = doc.data();
+  const img = cod(
+    await urlStorage(doc.id));
+  const alumno =
+    await buscaAlumno(
+      data.alumnoId);
+  const roles =
+    await buscaRoles(data.rolIds);
+  const parámetros =
+    new URLSearchParams();
+  parámetros.append("id", doc.id);
+  return (/* html */
+    `<li>
+      <a class="fila conImagen"
+          href=
+    "usuario.html?${parámetros}">
+        <span class="marco">
+          <img src="${img}"
+            alt="Falta el Avatar">
+        </span>
+        <span class="texto">
+          <strong
+              class="primario">
+            ${cod(doc.id)}
+          </strong>
+          <span
+              class="secundario">
+            ${alumno}<br>
+            ${roles}
+          </span>
+        </span>
+      </a>
+    </li>`);
+}
+
+/** Recupera el html de un
+ * alumno en base a su id.
+ * @param {string} id */
+async function
+  buscaAlumno(id) {
+  if (id) {
+    const doc =
+      await daoAlumno.
         doc(id).
         get();
-      if (doc.exists) {
-        const data = doc.data();
-        forma.cue.value = id || "";
-        img.src =
-          await urlStorage(id);
-        selectAlumnos(
-          forma.alumnoId,
-          data.alummnoId)
-        checksRoles(
-          listaRoles, data.rolIds);
-        forma.addEventListener(
-          "submit", guarda);
-        forma.eliminar.
-          addEventListener(
-            "click", elimina);
-      }
-    } catch (e) {
-      muestraError(e);
-      muestraUsuarios();
+    if (doc.exists) {
+      /**
+       * @type {import(
+          "./tipos.js").
+            Alumno} */
+      const data = doc.data();
+      return (/* html */
+        `${cod(data.nombre)}`);
     }
   }
-  
-  /** @param {Event} evt */
-  async function guarda(evt) {
-    await guardaUsuario(evt,
-      new FormData(forma), id);
-  }
-  
-  async function elimina() {
-    try {
-      if (confirm("Confirmar la " +
-        "eliminación")) {
-        await daoUsuario.
-          doc(id).delete();
-        await eliminaStorage(id);
-        muestraUsuarios();
-      }
-    } catch (e) {
-      muestraError(e);
+  return " ";
+}
+
+/** Recupera el html de los
+ * roles en base a sus id
+ * @param {string[]} ids */
+async function buscaRoles(ids) {
+  let html = "";
+  if (ids && ids.length > 0) {
+    for (const id of ids) {
+      const doc = await daoRol.
+        doc(id).
+        get();
+      /**
+       * @type {
+      import("./tipos.js").Rol} */
+      const data = doc.data();
+      html += /* html */
+        `<em>${cod(doc.id)}</em>
+        <br>
+        ${cod(data.descripción)}
+        <br>`;
     }
+    return html;
+  } else {
+    return "-- Sin Roles --";
   }
-  
+}
+
+/** @param {Error} e */
+function errConsulta(e) {
+  muestraError(e);
+  consulta();
+}
