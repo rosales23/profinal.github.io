bbf6794cb253b848c501449d0fb1788fa0355c19
rosales23 <rosales.upi@gmail.com>
2021-12-17 00:11:17 -0600

    Platillo

--- js/CtrlAlumnoNuevo.js
+++ js/CtrlAlumnoNuevo.js
@@ -15,7 +15,7 @@ import {
 
 const daoAlumno =
   getFirestore().
-    collection("Alumno");
+    collection("Platillo");
 /** @type {HTMLFormElement} */
 const forma = document["forma"];
 getAuth().onAuthStateChanged(
